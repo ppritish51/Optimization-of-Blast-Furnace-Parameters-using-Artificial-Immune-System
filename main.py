e2fd47216d9b87f64588e12110363da3302743b6
Pritish <pritish0509@gmail.com>
2019-10-16 02:02:27 +0530

    second commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from utility.decodedValue import decodeValue
 from dummy.dummyFunction import schaffer 
 
 
-population = [[1,0,1,0,0,1,1,0],[1,0,0,0,0,0,1,1,1]]
-x = decodeValue(population,-20,20)
+population = np.array([[[1,0,1,0,0,1,1,0],[1,0,0,0,0,0,1,1,1]]])
+x = decodeValue(population, np.array([-20]), np.array([20]))
 print('x',x)
 print(schaffer(x))
--- utility/decodedValue.py
+++ utility/decodedValue.py
@@ -1,26 +1,33 @@
 import numpy as np  
 
 def decodeValue(population,lowerBound,upperBound):
-	temp = []
+	dimension = len(population)
+	#print(dimension)
+	temp = [np.array([]) for i in range(dimension)]
+	#print(temp)
 
-	for member in population:
-		l = len(member)
-		val = 0
-		ind = 0
-		for ii in member[::-1]:
-			val = val + (2**ind)*ii
-			ind = ind + 1
-		
-		val  = lowerBound + (((upperBound - lowerBound) / (2**l -1)) * val) 
-		temp.append(val)
+	for i in range(dimension):
+		#print(i)
+		for member in population[i,:]:
+			l = len(member)
+			val = 0
+			ind = 0
+			for ii in member[::-1]:
+				val = val + (2**ind)*ii
+				ind = ind + 1
+			
+			val  = lowerBound[i] + (((upperBound[i] - lowerBound[i]) / (2**l -1)) * val) 
+			#print(member,' : ',val)
+			temp[i] = np.append(temp[i],val)
+			
+	return np.asarray(temp)
 
-	return np.asarray((temp))
+#### [x,y] , x and y are list
+##population = np.array([[[1,0,1,0,0,1,1,0],[1,0,0,0,0,0,1,1,1]]])
+##print(decodeValue(population, np.array([-20]), np.array([20])))
 
-
-population = [[1,0,1,0,0,1,1,0],[1,0,0,0,0,0,1,1,1]]
-print(decodeValue(population, -20, 20))
-
-
+##population = np.array([[[1,0,1,0,0,1,1,0],[1,0,0,0,0,0,1,1,1]],[[1,0,1,1,1,1,1,1],[1,0,0,0,0,0,0,0,0]]])
+##print(decodeValue(population, np.array([-20,0]), np.array([20,10])))
 
 
 
